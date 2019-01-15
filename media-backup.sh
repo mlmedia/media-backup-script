63584402ad82405eba6921605d0683226938439d
mlmedia <mike@millionlightsmedia.com>
2019-01-15 03:44:11 -0500

    - fix for non-working script?

--- media-backup.sh
+++ media-backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # generate the SITENAME for the AWS directory from the formatted host
 # - lowercase with hyphens only
 HOSTNAME="$(hostname -f)";
@@ -9,14 +8,9 @@ SITENAME=`echo ${HOSTNAME} | tr [:upper:] [:lower:] | tr -c '[:alnum:]' '-' | tr
 # sync with Amazon S3 using the CLI (with server side encryption)
 # install AWS CLI with instructions found here: https://linuxconfig.org/install-aws-cli-on-ubuntu-18-04-bionic-beaver-linux
 # check for existence of environment variable
-if [ -z "$MEDIADIR" ]
+if [ -z "$MEDIADIR" || -z "$S3MEDIABUCKET" ]
 then
-	echo "You need to set an ENVIRONMENT variable for the target MEDIADIR (e.g. export MEDIADIR=/var/www/yoursite/uploads)";
-	if [ -z "$S3MEDIABUCKET" ]
-	then
-		echo "You need to set an ENVIRONMENT variable for the target S3MEDIABUCKET (e.g. export S3MEDIABUCKET=my-bucket-name)";
-	else
-		echo "Syncing now...";
-		aws s3 sync $MEDIADIR s3://$S3MEDIABUCKET/$SITENAME --sse;
-	fi
+	echo "You need to set ENVIRONMENT variables for the target MEDIADIR (e.g. MEDIADIR=/var/www/yoursite/uploads) and for the target S3MEDIABUCKET variable (e.g. S3MEDIABUCKET=my-bucket-name)";
+else
+	aws s3 sync $MEDIADIR s3://$S3MEDIABUCKET/$SITENAME --sse;
 fi
